c39686d515215abf74e2d493d77377fb9a1e7e7e
AnKulai <116018047+AnKulai@users.noreply.github.com>
2023-04-10 21:39:40 +0300

    First_hw

---
+++ contacts.js
@@ -0,0 +1,40 @@
+import { readFile, writeFile } from "fs/promises";
+import { nanoid } from "nanoid";
+import path from "path";
+import { stringify } from "querystring";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+export const listContacts = async () => {
+  const data = await readFile(contactsPath);
+  //   console.log(JSON.parse(data));
+  return JSON.parse(data);
+};
+
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const selectContact = contacts.find((contact) => contact.id === contactId);
+  return console.log(selectContact ?? -1);
+};
+
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const removeIndex = contacts.findIndex(({ id }) => id === contactId);
+  if (removeIndex === -1) return null;
+  const [result] = contacts.splice(removeIndex, 1);
+  await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  console.log(`${result.name} was deleted`);
+  return result;
+};
+
+export const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const id = nanoid();
+  contacts.push({ id, name, email, phone });
+  await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return console.log(contacts);
+};
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+`node index.js --action="list"`
+
+![all-contacts](./screen/part_1_1.jpg)
+
+# Отримуємо контакт по id
+
+`node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6`
+
+![get-contact-by-id](./screen/part_1_2.jpg)
+
+# Додаємо контакт
+
+`node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22`
+
+![add-contact-by-id](./screen/part_1_3.jpg)
+
+# Видаляємо контакт
+
+`node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH`
+
+![remove-contact-by-id](./screen/part_1_2.jpg)
